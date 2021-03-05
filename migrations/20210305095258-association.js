8e1b2d180561bb7d35ff671ac5e7dcc8048f1fd3
senam86 <senameagblevon@gmail.com>
2021-03-05 11:52:44 +0100

    associations complete

---
+++ migrations/20210305095258-association.js
@@ -0,0 +1,65 @@
+'use strict';
+
+    module.exports = {
+      up: (queryInterface, Sequelize) => {
+        return queryInterface.addColumn(
+          "Requests", // name of Source model
+          "BuyerId", // nameof the key we're adding
+          {
+            type: Sequelize.INTEGER,
+            references: {
+              model: "Users", // name of Target model
+              key: "id", // key in Target model that we're referencing
+            },
+            onUpdate: "CASCADE",
+            onDelete: "SET NULL", 
+          }
+        ),
+        queryInterface.addColumn(
+          "Requests", // name of Source model
+          "SellerId", // nameof the key we're adding
+          {
+            type: Sequelize.INTEGER,
+            references: {
+              model: "Users", // name of Target model
+              key: "id", // key in Target model that we're referencing
+            },
+            onUpdate: "CASCADE",
+            onDelete: "SET NULL", 
+          }
+        ), 
+        queryInterface.addColumn(
+          "Requests", // name of Source model
+          "serviceId", // nameof the key we're adding
+          {
+            type: Sequelize.INTEGER,
+            references: {
+              model: "Services", // name of Target model
+              key: "id", // key in Target model that we're referencing
+            },
+            onUpdate: "CASCADE",
+            onDelete: "SET NULL", 
+          }
+        );   
+    },
+    
+    
+
+  
+    down: (queryInterface, Sequelize) => {
+      return queryInterface.removeColumn(
+        "Requests", // name of Source model
+        "BuyerId" // key we want to remove
+      ),
+      queryInterface.removeColumn(
+        "Requests", // name of Source model
+        "SellerId" // key we want to remove
+      ),
+      queryInterface.removeColumn(
+        "Requests", // name of Source model
+        "serviceId" // key we want to remove
+      );
+    }
+  };
+
+ 
---
+++ migrations/20210305102916-association.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn(
+      "Services", // name of Source model
+      "UserId", // nameof the key we're adding
+      {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users", // name of Target model
+          key: "id", // key in Target model that we're referencing
+        },
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL", 
+      }
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn(
+      "Services", // name of Source model
+      "UserId" // key we want to remove
+    );
+  }
+};
