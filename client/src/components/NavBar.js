e628e5ea59021702fc3639d8b490f9acbdec3a65
Sofia Moura <sofiamourap@gmail.com>
2021-03-10 17:30:40 +0000

    condition for the navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 export default function NavBar() {
+  const auth = useAuth();
   return (
     <div>
       <NavLink to="/home">Home</NavLink>
       <NavLink to="/login">Login</NavLink>
       <NavLink to="/register">Register</NavLink>
       <NavLink to="/profile">Profile</NavLink>
+
+      <div>
+        {auth && (
+          <div>
+            <NavLink to="">logout</NavLink>
+            <NavLink to="/wallet">Balance: 2h</NavLink>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
