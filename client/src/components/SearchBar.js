48cd6f731973cc425e86e169c3c3a1ac6b56dcad
Sofia Moura <sofiamourap@gmail.com>
2021-03-14 11:17:02 +0000

    fix issue toLowerCase

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -2,27 +2,35 @@ import React, { useState, useEffect } from "react";
 import api from "../data/index.js";
 
 export default function SearchBar() {
-  const [keyword, setKeyword] = useState();
+  // const [keyword, setKeyword] = useState();
+  // const [services, setServices] = useState([]);
+  // const [servicesDefault, setServicesDefault] = useState([]);
+
+  // const handleChange = (e) => {
+  //   setKeyword(e.target.value);
+  //   updateInput(e.target.value);
+  // };
+
+  // const updateInput = async (input) => {
+  //   const filtered = servicesDefault.filter((service) => {
+  //     return service.servicename.toLowerCase().includes(input.toLowerCase());
+  //   });
+  //   setKeyword(input);
+  //   setServices(filtered);
+  // };
+  const [searchTerm, setSearchTerm] = useState("");
   const [services, setServices] = useState([]);
-  const [servicesDefault, setServicesDefault] = useState([]);
 
-  const handleChange = (e) => {
-    setKeyword(e.target.value);
-    updateInput(e.target.value);
-  };
-
-  const updateInput = async (input) => {
-    const filtered = servicesDefault.filter((service) => {
-      return service.servicename.toLowerCase().includes(input.toLowerCase());
-    });
-    setKeyword(input);
-    setServices(filtered);
-  };
+  // useEffect(async () => {
+  //   const result = await api.getServices();
+  //   setServices(result.data);
+  //   // setServicesDefault(result.data);
+  //   // console.log(result);
+  // }, []);
 
   useEffect(async () => {
     const result = await api.getServices();
     setServices(result.data);
-    setServicesDefault(result.data);
     // console.log(result);
   }, []);
 
@@ -30,24 +38,35 @@ export default function SearchBar() {
     <div>
       <input
         key="random1"
-        value={keyword}
         placeholder={"search service"}
-        onChange={handleChange}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
       <button className="btn btn-dark" onClick="">
         Search
       </button>
       <>
-        {services.map((data, index) => {
-          if (data) {
-            return (
-              <div key={data.id}>
-                <h1>{data.servicename}</h1>
-              </div>
-            );
-          }
-          return null;
-        })}
+        {services
+          .filter((s) => {
+            if (searchTerm === "") {
+              return s;
+            } else if (
+              (s.servicename ? s.servicename : "")
+                .toLowerCase()
+                .includes(searchTerm.toLowerCase())
+            ) {
+              return s;
+            }
+          })
+          .map((data) => {
+            if (data) {
+              return (
+                <div key={data.id}>
+                  <h1>{data.servicename}</h1>
+                </div>
+              );
+            }
+            return null;
+          })}
       </>
     </div>
   );
