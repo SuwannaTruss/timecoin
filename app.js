cd698fd1db3ae7d28fbee423cc896b6b04b29991
Sofia Moura <sofiamourap@gmail.com>
2021-03-15 15:08:44 +0000

    changing code in the app.js folder to deploy in heroku

--- app.js
+++ app.js
@@ -16,13 +16,19 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/services", servicesRouter);
 app.use("/categories", categoriesRouter);
 app.use("/requests", requestsRouter);
+
+//instructions from heroku README file
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
